a3a90a32780dd2467c217b9f17de39686ff60ef7
Micheal Koblinski <stevenkoblinskip@gmail.com>
2020-02-19 23:20:36 -0500

    added to the login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,8 +2,60 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Box className={classes.center}>
+      {console.log(TextField)}
+      <Box bgcolor='success.main' border={1} className={classes.signUp}>
+        <Typography variant="h4" component="h2" color='secondary' className={classes.signUpTitle}>
+          Registration
+        </Typography>
+        <div>
+        <TextField
+          required
+          id="standard-basic"
+          label="Username"
+          autoFocus
+          color='secondary'
+          className={classes.inputFields}
+          inputProps={{
+            color: 'white'
+          }}
+        />
+        <TextField
+          required
+          id="standard-basic"
+          label="Email"
+          color='secondary'
+          className={classes.inputFields}
+          InputProps={{
+            className: classes.input
+          }}
+        />
+        <TextField
+          required
+          id="standard-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          color='secondary'
+          className={classes.inputFields}
+          InputProps={{
+            className: classes.input
+          }}
+        />
+        <TextField
+          required
+          id="standard-password-input"
+          label="Comfirm Password"
+          type="password"
+          autoComplete="current-password"
+          color='secondary'
+          className={classes.inputFields}
+          InputProps={{
+            className: classes.input
+          }}
+        />
+      </div>
+      </Box>
+    </Box>
   )
 }
