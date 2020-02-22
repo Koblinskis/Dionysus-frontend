c03b984d4c442083449dd554367240004604f571
Micheal Koblinski <stevenkoblinskip@gmail.com>
2020-02-22 18:06:50 -0500

    small styling

--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import { Box, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  signUp: {
+    margin: '30px',
+    maxWidth: 400,
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '40px',
+    [theme.breakpoints.down('xs')]: {
+      padding: '20px',
+      maxWidth: 200,
+    },
+    alignContent: 'center',
+    textAlign: 'center',
+  },
+  center: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+}))
 
 export default function Settings() {
+  const classes = useStyles()
   return (
-    <Box>
-      <Typography variant='h2' color='secondary'>Settings</Typography>
+    <Box className={classes.center}>
+      <Box bgcolor='success.main' border={1} className={classes.signUp}>
+        <Typography variant='h2' color='secondary'>Settings</Typography>
+
+      </Box>
     </Box>
   )
 }
