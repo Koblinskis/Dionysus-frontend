7f81ef1f9e8a02e72e67b96410476b7492cb2e34
Micheal Koblinski <stevenkoblinskip@gmail.com>
2020-03-20 14:33:49 -0400

    implemented the new component doesn't save but is moew effiecent

--- src/components/AddItemPage.js
+++ src/components/AddItemPage.js
@@ -1,10 +1,8 @@
 import React, { useEffect } from 'react'
-import { Box, Typography, FormControl, Select, MenuItem, InputLabel, TextField, Grid, Button } from '@material-ui/core';
+import { Box, Typography, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
-import { useCookies } from 'react-cookie'
-import { v4 as uuidv4 } from 'uuid'
-import Lists from './Lists'
+import ListItem from './listItem'
 
 const useStyles = makeStyles(theme => ({
   formControl: {
@@ -34,32 +32,11 @@ const useStyles = makeStyles(theme => ({
     justifyContent: 'center'
   },
 }))
-let counter = 0
 
 export default function AddItemPage() {
   const classes = useStyles()
-  const id = uuidv4();
-  const [cookie, setCookie]= useCookies()
-  const [dataStructure, setDataStructure] = React.useState('');
   const [form, setForm] = React.useState(undefined)
-  const [listItemsGrid, setListItemsGrid] = React.useState(new Map());
-  const [listItems, setListItems] = React.useState([])
-  const [items, setItems] = React.useState([])
-  const [description, setDescription] = React.useState([])
-  const [confirm, setConfirm] = React.useState(false)
-
-  // useEffect(() => {
-  //   setListItems(listItems.set(id, (<Grid item md={4} sm={12} xs={12} key={uuidv4()}>
-  //     <TextField key={uuidv4()} color="secondary" id="standard-basic" label={`Item`} />
-  //     <br/>
-  //     <TextField key={uuidv4()} color="secondary" id="standard-multiline-flexible" multiline rowsMax="4" label={`Decsription`} />
-  //   </Grid>)))
-  // }, [])
-
-  const handleChange = async event => {
-    setDataStructure(event.target.value);
-    chosenData(event.target.value)
-  };
+  const [number, setNumber] = React.useState(0)
 
   const chosenData = (dataStructure) => {
     switch (dataStructure) {
@@ -81,70 +58,23 @@ export default function AddItemPage() {
   const list = () => {
     return (
       <Box className={classes.listItems}>
-        <Grid container spacing={6} >
-          {[...listItemsGrid.keys()].map(k => listItemsGrid.get(k))}
-        </Grid>
+          {<ListItem number={number}/>}
         <br/>
-        <Button color="secondary" onClick={addItemsList}>
+        <Button color="secondary" onClick={addItem}>
             <AddCircleIcon />Add Item
         </Button>
       </Box>
     )
   }
 
-  const addItemsList = () => {
-    let itemsGrid = (<Grid item md={4} sm={12} xs={12} key={uuidv4()}>
-      <TextField key={uuidv4()} color="secondary" id="standard-basic" label={`Item`} defaultValue={items[counter]} onChange={(e) => handleItemChange(counter, e)}/>
-      <br/>
-      <TextField key={uuidv4()} color="secondary" id="standard-multiline-flexible" multiline rowsMax="4" label={`Description`} defaultValue={description[counter]} onChange={(e) => handleDescriptionChange(counter, e)}/>
-    </Grid>)
-    const newMap = new Map(listItemsGrid.set(counter, itemsGrid))
-    setListItemsGrid(newMap)
-    counter++;
-  }
-
-  const saveListInfo = async () => {
-    for (let i = 0; i < items.length; i++){
-      setListItems(listItems.concat({item: items[i], description: description[i]}))
-      setConfirm(true)
-    }
-  }
-
-  const saveToDatabase = async () => {
-    await postList(listItems)
-    setConfirm(false)
-  }
-
-  const handleItemChange = (index, e) => {
-    setItems(items.concat(e.target.value))
-  }
-
-  const handleDescriptionChange = (index, e) => {
-    setDescription(description.concat(e.target.value))
-  }
-
-  const postList = async (listItems) => {
-    try {
-      const res = await fetch(process.env.REACT_APP_NODE_URL + 'addlist', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + cookie.token,
-        },
-        body: JSON.stringify(listItems)
-      })
-      return res
-    } catch (e) {
-      console.error('Error:', e)
-      alert(e)
-    }
+  const addItem = () => {
+    setNumber(number => number + 1)
   }
 
   return (
     <Box className={classes.center}>
       <Box bgcolor='success.main' border={1} className={classes.formControl}>
         <Typography variant='h3' color='secondary'>List</Typography>
-        {console.log(listItems)}
       {/* <FormControl>
         <InputLabel id="demo-simple-select-label" color="secondary">Type of field</InputLabel>
         
@@ -161,11 +91,6 @@ export default function AddItemPage() {
       </FormControl> */}
       {/* {dataStructure ? form : undefined} */}
         {list()}
-        {confirm ? 
-        <Button variant='contained' color="primary" onClick={saveToDatabase}>Confirm Save</Button>
-        :
-        <Button variant='contained' onClick={saveListInfo}>Save List</Button>
-        }
       </Box>
     </Box>
   )
--- src/components/listItem.js
+++ src/components/listItem.js
@@ -4,12 +4,21 @@ import { Box, TextField, Grid } from '@material-ui/core';
 
 
 export default function ListItem(props) {
-  return (
-    <Grid item md={4} sm={12} xs={12} key={uuidv4()}>
-      <TextField key={uuidv4()} color="secondary" id="standard-basic" label={`Item`}/>
+
+  const [item, setItems] = React.useState([])
+
+
+  React.useEffect(() => {
+    setItems(item.concat(<Grid item md={4} sm={12} xs={12} key={props.number}>
+      <TextField key={props.number} color="secondary" id="standard-basic" label={`Item`}/>
       <br/>
-      <TextField key={uuidv4()} color="secondary" id="standard-multiline-flexible" multiline rowsMax="4" label={`Description`}/>
+      <TextField key={props.number} color="secondary" id="standard-multiline-flexible" multiline rowsMax="4" label={`Description`}/>
+    </Grid>))
+  }, [props.number])
+
+  return (
+    <Grid container spacing={6} >
+      {item}
     </Grid>
   )
 }
-
